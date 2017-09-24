6f6d7c124825c48484be9a1ad464ff048ad1ec9b
Kelvin <et.kelvin@gmail.com>
2017-09-24 23:40:03 +0800

    add custom table view cells programmatically

---
+++ Table Cells/Table Cells/NameAndColorCell.swift
@@ -0,0 +1,71 @@
+//
+//  NameAndColorCell.swift
+//  Table Cells
+//
+//  Created by Kelvin Leung on 24/09/2017.
+//  Copyright © 2017 Kelvin Leung. All rights reserved.
+//
+
+import UIKit
+
+class NameAndColorCell: UITableViewCell {
+    
+    var name: String = "" {
+        didSet {
+            if (name != oldValue) {
+                nameLabel.text = name
+            }
+        }
+    }
+    var color: String = "" {
+        didSet {
+            if (color != oldValue) {
+                colorLabel.text = color
+            }
+        }
+    }
+    var nameLabel: UILabel!
+    var colorLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        let nameLabelRect = CGRect(x: 0, y: 5, width: 70, height: 15)
+        let nameMarker = UILabel(frame: nameLabelRect)
+        nameMarker.textAlignment = .right
+        nameMarker.text = "Name:"
+        nameMarker.font = UIFont.boldSystemFont(ofSize: 12)
+        contentView.addSubview(nameMarker)
+        
+        let colorLabelRect = CGRect(x: 0, y: 26, width: 70, height: 15)
+        let colorMarker = UILabel(frame: colorLabelRect)
+        colorMarker.textAlignment = .right
+        colorMarker.text = "Color:"
+        colorMarker.font = UIFont.boldSystemFont(ofSize: 12)
+        contentView.addSubview(colorMarker)
+        
+        let nameValueRect = CGRect(x: 80, y: 5, width: 200, height: 15)
+        nameLabel = UILabel(frame: nameValueRect)
+        contentView.addSubview(nameLabel)
+        
+        let colorValueRect = CGRect(x: 80, y: 26, width: 200, height: 15)
+        colorLabel = UILabel(frame: colorValueRect)
+        contentView.addSubview(colorLabel)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Table Cells/Table Cells/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Table Cells
+//
+//  Created by Kelvin Leung on 24/09/2017.
+//  Copyright © 2017 Kelvin Leung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    let cellTableIdentifier = "CellTableIdentifier"
+    let computers = [
+        ["Name" : "MacBook Air", "Color" : "Silver"],
+        ["Name" : "MacBook Pro", "Color" : "Silver"],
+        ["Name" : "iMac", "Color" : "Silver"],
+        ["Name" : "Mac Mini", "Color" : "Silver"],
+        ["Name" : "Mac Pro", "Color" : "Black"]
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.register(NameAndColorCell.self, forCellReuseIdentifier: cellTableIdentifier)
+    }
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return computers.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellTableIdentifier, for: indexPath) as! NameAndColorCell
+        let rowData = computers[indexPath.row]
+        cell.name = rowData["Name"]!
+        cell.color = rowData["Color"]!
+        return cell
+    }
+}
