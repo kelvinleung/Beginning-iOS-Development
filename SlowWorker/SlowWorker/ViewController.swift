3ea8d6c720f231c14f2bd082d6002fcd6c49165c
Kelvin <et.kelvin@gmail.com>
2017-10-19 23:21:59 +0800

    GCD

---
+++ SlowWorker/SlowWorker/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  SlowWorker
+//
+//  Created by Kelvin Leung on 19/10/2017.
+//  Copyright © 2017 Kelvin Leung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var startButton: UIButton!
+    @IBOutlet var resultsTextView: UITextView!
+    @IBOutlet var spinner: UIActivityIndicatorView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func fetchSomethingFromServer() -> String {
+        Thread.sleep(forTimeInterval: 1)
+        return "Hi there"
+    }
+    
+    func processData(_ data: String) -> String {
+        Thread.sleep(forTimeInterval: 2)
+        return data.uppercased()
+    }
+    
+    func calculateFirstResult(_ data: String) -> String {
+        Thread.sleep(forTimeInterval: 3)
+        return "Number of chars: \(data.characters.count)"
+    }
+    
+    func calculateSecondResult(_ data: String) -> String {
+        Thread.sleep(forTimeInterval: 4)
+        return data.replacingOccurrences(of: "E", with: "e")
+    }
+    
+    @IBAction func doWork(_ sender: UIButton) {
+        let startTime = Date()
+        resultsTextView.text = ""
+        // Enhance user experience
+        startButton.isEnabled = false
+        spinner.startAnimating()
+        let queue = DispatchQueue.global(qos: .default)
+        queue.async {
+            let fetchedData = self.fetchSomethingFromServer()
+            let processedData = self.processData(fetchedData)
+            let firstResult = self.calculateFirstResult(processedData)
+            let secondResult = self.calculateSecondResult(processedData)
+            let resultsSummary = "First: [\(firstResult)]\nSecond: [\(secondResult)]"
+            // UI must be used from main thread only
+            DispatchQueue.main.async {
+                self.resultsTextView.text = resultsSummary
+                self.startButton.isEnabled = true
+                self.spinner.stopAnimating()
+            }
+            let endTime = Date()
+            print("Completed in \(endTime.timeIntervalSince(startTime)) seconds")
+        }
+    }
+    
+}
