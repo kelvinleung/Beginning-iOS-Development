c3842e8ba818d9e61507348fbf9e5e4245499afd
Kelvin <et.kelvin@gmail.com>
2017-10-25 23:47:13 +0800

    TouchExplorer

---
+++ TouchExplorer/TouchExplorer/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  TouchExplorer
+//
+//  Created by Kelvin Leung on 25/10/2017.
+//  Copyright © 2017 Kelvin Leung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var tapsLabel: UILabel!
+    @IBOutlet weak var touchesLabel: UILabel!
+    @IBOutlet weak var forceLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
