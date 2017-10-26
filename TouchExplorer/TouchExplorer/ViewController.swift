2049b447f06c7a8d01c6d09ede9d361a0f63b7f0
Kelvin <et.kelvin@gmail.com>
2017-10-27 01:20:32 +0800

    detect touches

--- TouchExplorer/TouchExplorer/ViewController.swift
+++ TouchExplorer/TouchExplorer/ViewController.swift
@@ -19,5 +19,43 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    private func updataLabelsFromTouches(_ touch: UITouch?, allTouches: Set<UITouch>?) {
+        let numTaps = touch?.tapCount ?? 0
+        let tapsMessage = "\(numTaps) taps detected"
+        tapsLabel.text = tapsMessage
+        
+        let numTouches = allTouches?.count ?? 0
+        let touchesMessage = "\(numTouches) touches detected"
+        touchesLabel.text = touchesMessage
+        
+        if traitCollection.forceTouchCapability == .available {
+            forceLabel.text = """
+            Force: \(touch?.force ?? 0)
+            Max force: \(touch?.maximumPossibleForce ?? 0)
+            """
+        } else {
+            forceLabel.text = "3D Touch not availabel"
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        messageLabel.text = "Touches Began"
+        updataLabelsFromTouches(touches.first, allTouches: event?.allTouches)
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        messageLabel.text = "Touches Cancelled"
+        updataLabelsFromTouches(touches.first, allTouches: event?.allTouches)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        messageLabel.text = "Touches Moved"
+        updataLabelsFromTouches(touches.first, allTouches: event?.allTouches)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        messageLabel.text = "Touches Ended"
+        updataLabelsFromTouches(touches.first, allTouches: event?.allTouches)
+    }
+    
 }
-
